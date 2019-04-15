c309e878f223e8196a6caf13132e7037cc38d75d
ameliadoetsch <doetschamelia@gmail.com>
2019-04-14 23:07:07 -0400

    this seems correct now

--- part1a.py
+++ part1a.py
@@ -2,22 +2,6 @@ import numpy as np
 import random as random
 import matplotlib.pyplot as plt
 
-def check_adjacent(lattice, point):
-    x = point[0]
-    y = point[1]
-    if x + 1 < 250 and y + 1 < 250 and x - 1 > 0 and y - 1 > 0:
-        if lattice[x + 1][y] == 1:
-            lattice[x][y] = 1
-        elif lattice[x - 1][y] == 1:
-            lattice[x][y] = 1
-        elif lattice[x][y + 1] == 1:
-            lattice[x][y] = 1
-        elif lattice[x][y - 1] == 1:
-            lattice[x][y] = 1
-        else:
-            lattice[x][y] = -1
-    return lattice
-
 
 def random_walk(point):
     x = point[0]
@@ -35,6 +19,7 @@ def random_walk(point):
     point[1] = y
     return point
 
+
 r = 100
 # origin at (125, 125); i = [0, 250] j = [0, 250]
 
@@ -45,14 +30,17 @@ lattice[125][125] = 1
 circle = []
 i = 0
 j = 0
+
 # create list of perimeter points
 for i in range(250):
     for j in range(250):
         radius = np.sqrt((125-i)**2 + (125-j)**2)
+        # there is a little leeway (+/- 1) for creating the list of perimeter points to create a
+        # large sampling of points at or near the perimeter. there were very few points that had a radius of
+        # exactly 100, which leaves room for additional errors
         if radius < 101 and radius > 99:
             circle.append([i, j])
 
-print(circle)
 elements_in_circle = len(circle)
 circle_index_max = elements_in_circle - 1
 
@@ -61,7 +49,6 @@ isSize = False
 while isSize is False:
     # place new particle at random point along perimeter
     rand = random.randint(0, circle_index_max)
-    print(rand)
     point = circle[rand]
     x_old = (circle[rand])[0]
     y_old = (circle[rand])[1]
@@ -70,11 +57,21 @@ while isSize is False:
     escape = False
 
     # check if adjacent points are occupied
-    for i in range(250):
-        for j in range(250):
-            lattice[i][j] = (check_adjacent(lattice, point))[i][j]
-    if lattice[x_old][y_old] == 1:
-        isAdded = True
+    if x_old + 1 < 250 and y_old + 1 < 250 and x_old - 1 > 0 and y_old - 1 > 0:
+        if lattice[x_old + 1][y_old] == 1:
+            lattice[x_old][y_old] = 1
+            isAdded = True
+        elif lattice[x_old - 1][y_old] == 1:
+            lattice[x_old][y_old] = 1
+            isAdded = True
+        elif lattice[x_old][y_old + 1] == 1:
+            lattice[x_old][y_old] = 1
+            isAdded = True
+        elif lattice[x_old][y_old - 1] == 1:
+            lattice[x_old][y_old] = 1
+            isAdded = True
+        else:
+            lattice[x_old][y_old] = -1
 
     while isAdded is False:
         point = random_walk(point)
@@ -84,19 +81,29 @@ while isSize is False:
             escape = True
             lattice[x_new][y_new] = 0
             break
-        lattice[x_new][y_new] = -1
-        lattice[x_old][y_old] = 0
+        else:
+            lattice[x_new][y_new] = -1
+            lattice[x_old][y_old] = 0
 
         # check if adjacent points are occupied
-        for i in range(250):
-            for j in range(250):
-                lattice[i][j] = (check_adjacent(lattice, point))[i][j]
-        if lattice[x_old][y_old] == 1:
-            isAdded = True
-        else:
-            x_old = x_new
-            y_old = y_new
-            point = [x_old, y_old]
+        if x_new + 1 < 250 and y_new + 1 < 250 and x_new - 1 > 0 and y_new - 1 > 0:
+            if lattice[x_new + 1][y_new] == 1:
+                lattice[x_new][y_new] = 1
+                isAdded = True
+            elif lattice[x_new - 1][y_new] == 1:
+                lattice[x_new][y_new] = 1
+                isAdded = True
+            elif lattice[x_new][y_new + 1] == 1:
+                lattice[x_new][y_new] = 1
+                isAdded = True
+            elif lattice[x_new][y_new - 1] == 1:
+                lattice[x_new][y_new] = 1
+                isAdded = True
+            else:
+                lattice[x_new][y_new] = 0
+                point = [x_new, y_new]
+                x_old = x_new
+                y_old = y_new
 
     np.savetxt("cluster.txt", lattice)
     # check is desired size is achieved
@@ -109,6 +116,7 @@ while isSize is False:
 
 np.savetxt("cluster.txt", lattice)
 
+# create 2 arrays storing x and y positions of cluster points
 x = []
 y = []
 for i in range(250):
@@ -117,6 +125,11 @@ for i in range(250):
             x.append(i)
             y.append(j)
 
-plt.scatter(x, y)
+plt.scatter(x, y, label="Cluster Particles")
+plt.xlabel("Horizontal Position")
+plt.ylabel("Vertical Position")
+plt.title("2D Cluster Growth Using the DLA Model")
+plt.legend()
+plt.save("C:/Users/doets/Desktop/project2A/_2DCluster.png")
 plt.show()
 
