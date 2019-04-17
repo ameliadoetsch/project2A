4f65fb29adb0ef58fe552a8631449e459be18ced
iowreth <48215832+iowreth@users.noreply.github.com>
2019-04-17 15:05:09 -0400

    Move check is better
    Still getting occasional error when it checks for with positions are open to move to.  Still have not implemented a check to see if the fish/shark has already moved on a time step.

--- part2.py
+++ part2.py
@@ -12,7 +12,7 @@ fish_move = [[0 for j in range(grid_size)] for i in range(grid_size)]
 shark_move = [[0 for j in range(grid_size)] for i in range(grid_size)]
 shark_starve = [[0 for j in range(grid_size)] for i in range(grid_size)]
 iter_array =  np.arange(0,grid_size,1)
-time_steps = np.arange(1, 365, 1)
+time_steps = np.arange(1, 5, 1)
 
 
 #Function to seed intial populations
@@ -31,42 +31,85 @@ def pop_seed(twoD_array, pop_Max):
 
 #function to check and randomly choose an adjacent unoccupied location
 def location_check_pick(twoD_array, max_size, x, y):
-    loc = random.randint(1,4)
-    if (loc == 1):
-        if ((y-1) > -1):
-            if twoD_array[x][max_size] > -1:
-                location_check_pick(twoD_array, max_size, x, y)
+    direction = ['up', 'right', 'down', 'left']
+
+    if (x == 0):
+        if twoD_array[max_size-1][y] >= 0:
+            direction.remove('up')
+    elif twoD_array[x-1][y] >= 0:
+        direction.remove('up')
+
+    if(y == max_size-1):
+        if twoD_array[x][0] >= 0:
+            direction.remove('right')
+    elif twoD_array[x][y+1] >= 0:
+        direction.remove('right')
+
+    if (x == max_size - 1):
+        if twoD_array[0][y] >= 0:
+            direction.remove('down')
+    elif twoD_array[x+1][y] >= 0:
+        direction.remove('down')
+
+    if (y == 0):
+        if twoD_array[x][max_size-1] >= 0:
+            direction.remove('left')
+    elif twoD_array[x][y - 1] >= 0:
+        direction.remove('left')
+
+    length = len(direction)
+    rand_loc = random.randint(0, length)
+
+    if length == 0:
+        new_row = x
+        new_col = y
+    else:
+        if direction[rand_loc] == 'up':
+            if (x-1 < 0):
+                new_row = max_size -1
             else:
-                return "left"
-        elif twoD_array[x][y-1] > -1:
-            location_check_pick(twoD_array, max_size, x, y)
-        else:
-            return "left"
-    elif (loc == 2):
-        if ((x+1) > max_size):
-            if twoD_array[0][y] > -1:
-                location_check_pick(twoD_array, max_size, x, y)
+                new_row = x - 1
+            new_col = y
+        elif direction[rand_loc] == 'right':
+            new_row = x
+            if (y + 1 > max_size - 1):
+                new_col = 0
             else:
-                return "down"
-        elif twoD_array[x+1][y] > -1:
-                location_check_pick(twoD_array, max_size, x, y)
-        else:
-            return "down"
-    elif (loc == 3):
-        if ((y+1) > max_size):
-            if twoD_array[x][0] > -1:
-                location_check_pick(twoD_array, max_size, x, y)
+                new_col = y + 1
+        elif direction[rand_loc] == 'down':
+            if (x + 1 > max_size -1):
+                new_row = 0
             else:
-                return "right"
-        elif twoD_array[x][y] > -1:
-                location_check_pick(twoD_array, max_size, x, y)
-        else:
-            return "right"
+                new_row = x + 1
+            new_col = y
+        elif direction[rand_loc] == 'left':
+            new_row = x
+            if (y -1 < 0):
+                new_col = max_size -1
+            else:
+                new_col = y - 1
+    return new_row, new_col
 
+pop_seed(fish_positions, 2)
 
-# for t in time_steps:
-#     for i in iter_array:
-#         for j in iter_array:
-#            if fish_positions[i][j] != -1:
+for row in fish_positions:
+    print(row)
 
+print('')
 
+pot_change = 0
+
+for t in time_steps:
+    for i in iter_array:
+        for j in iter_array:
+           if fish_positions[i][j] >= 0:
+                move_i, move_j = location_check_pick(fish_positions, grid_size, i, j)
+                if move_i == i and move_j == j:
+                    fish_positions[move_i][move_j] = fish_positions[i][j] + 1
+                else:
+                    fish_positions[move_i][move_j] = fish_positions[i][j] + 1
+                    fish_positions[i][j] = -1
+                pot_change = pot_change + 1
+    for row in fish_positions:
+        print(row)
+    print(pot_change)
